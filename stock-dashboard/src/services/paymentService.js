1edd37b672574dbad299315619e55e44732267e7
Deaksh <Deakshanth.shetty@gmail.com>
2025-08-08 00:05:31 +0530

    paymentService.js

--- stock-dashboard/src/services/paymentService.js
+++ stock-dashboard/src/services/paymentService.js
@@ -1,5 +1,5 @@
 export const createOrder = async (uid, credits) => {
-  const response = await fetch("https://stock-indicator-alert-1.onrender.com/create_order", {
+  const response = await fetch("https://stock-indicator-alert-1.onrender.com/payment/create_order", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ uid: uid, credits: credits }),
