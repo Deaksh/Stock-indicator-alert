15b67d3de4820ecb359eca1217cb83094dcd4211
Deaksh <Deakshanth.shetty@gmail.com>
2025-08-07 23:36:21 +0530

    App.js

--- stock-dashboard/src/App.js
+++ stock-dashboard/src/App.js
@@ -35,7 +35,7 @@ const AVAILABLE_INDICATORS = [
   { key: "rsi", label: "RSI" },
 ];
 
-const BACKEND_URL = "https://stock-indicator-alert.onrender.com";
+const BACKEND_URL = "https://stock-indicator-alert-1.onrender.com";
 
 export default function App() {
   // Auth states
